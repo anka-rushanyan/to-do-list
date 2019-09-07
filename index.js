d677350a0669be6bcc116af4682d207b1d76ce44
anka-rushanyan <ankarushanyan99@gmail.com>
2019-09-07 13:15:02 +0100

    My application of to do list

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var i;
      		this.classList.toggle('checked')
  }
 }
+
 var close = document.getElementsByClassName('close');
 var i;
 	for (i = 0; i < myList.length; i++) {
@@ -22,30 +23,31 @@ var i;
        		 div.style.display = 'none';
     }
 }
+
 function myFunc() {
- var li = document.createElement('li');
- var inp = document.getElementById('input').value;
- var val = document.createTextNode(inp);
- 	 li.appendChild(val);
-    	 if (inp === '') {
-   			alert('Write something please!');
-     }
-    	 else {
-   			document.getElementById('ul').appendChild(li);
-     }
- var span = document.createElement('SPAN');
- var txt = document.createTextNode('X');
- 	 span.className = 'close';
- 	 span.appendChild(txt);
- 	 li.appendChild(span);
-  		document.getElementById('input').value = '';
-   		li.onclick = function () {
-        this.classList.toggle('checked')
-   };
-	 for (i = 0; i < close.length; i++) {
-  		close[i].onclick = function() {
-     var div = this.parentElement;
-    	 div.style.display = "none";
-   }
- }
+	var li = document.createElement('li');
+	var inp = document.getElementById('input').value;
+	var val = document.createTextNode(inp);
+		li.appendChild(val);
+			if (inp === '') {
+				alert('Write something please!');
+		}
+			else {
+				document.getElementById('ul').appendChild(li);
+		}
+	var span = document.createElement('SPAN');
+	var txt = document.createTextNode('X');
+		span.className = 'close';
+		span.appendChild(txt);
+		li.appendChild(span);
+			document.getElementById('input').value = '';
+			li.onclick = function () {
+			this.classList.toggle('checked')
+	};
+		for (i = 0; i < close.length; i++) {
+			close[i].onclick = function() {
+		var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
 }
